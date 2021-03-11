9640419c99eeefcf04a6710c9eb37c13f28f3ed2
Allison Edwards <aedwards625162@users.noreply.github.com>
2021-03-11 12:30:54 -0600

    Made days of the week consecutive

--- index.js
+++ index.js
@@ -18,6 +18,41 @@ let days = [
   currentDay.innerHTML = `${weekDay}`;
   currentTime.innerHTML = `${time}`;
   
+  function displayForecast (response) {
+    let days = [
+    "Sun.",
+    "Mon.",
+    "Tues.",
+    "Wed.",
+    "Thurs.",
+    "Fri.",
+    "Sat.",
+    "Sun.",
+    "Mon.",
+    "Tues.",
+    "Wed.",
+    "Thurs.",
+    "Fri.",
+    "Sat.",
+    "Sun."
+  ];
+     let daySlotOne = days[now.getDay() + 1]; 
+     let daySlotTwo = days[now.getDay() + 2]; 
+     let daySlotThree = days[now.getDay() + 3]; 
+     let daySlotFour = days[now.getDay() + 4]; 
+     let daySlotFive = days[now.getDay() + 5]; 
+     let dayOne = document.querySelector("#dayOne");
+     let dayTwo = document.querySelector("#dayTwo");
+     let dayThree = document.querySelector("#dayThree");
+     let dayFour = document.querySelector("#dayFour");
+     let dayFive = document.querySelector("#dayFive");
+     dayOne.innerHTML = `${daySlotOne}`;
+    dayTwo.innerHTML = `${daySlotTwo}`;
+    dayThree.innerHTML = `${daySlotThree}`;
+    dayFour.innerHTML = `${daySlotFour}`;
+    dayFive.innerHTML = `${daySlotFive}`;
+    
+  }
   
   function showWeather(event) {
       event.preventDefault();
@@ -28,6 +63,9 @@ let days = [
       let apiUrl =
       `https://api.openweathermap.org/data/2.5/weather?q=${searchCity.value}&units=imperial&appid=${apiKey}`;
       axios.get(`${apiUrl}`).then(showTemperature);
+
+      apiUrlForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCity.value}&appid=${apiKey}&units=imperial`;
+      axios.get(apiUrlForecast).then(displayForecast);
   }
   
   let input = document.querySelector("#city-form");
